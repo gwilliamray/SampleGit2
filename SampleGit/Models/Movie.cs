55ae3d222fb06fdb4639a0fc578eff608684c248
Glenn Ray <gray@southeast.edu>
2020-03-18 16:40:54 -0500

    Add project files.

---
+++ SampleGit/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleGit.Models
+{
+    public class Movie
+    {
+        // add more content here later
+
+    }
+}
